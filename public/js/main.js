0bf128b143a05ba05ffc6d4d637ac481936464c7
Andrew Perotti <Perotti28@gmail.com>
2024-06-28 13:26:05 -0700

    Removed the complete on click or uncomplete on click functionality from items, as users can delete from the request page and complete on click might be triggered accidently. I could later add delete buttons next to items that belongs to the requested user but want to keep them on the request page for now.

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
 
 const deleteBtn = document.querySelectorAll('.del')
-const deleteJobBtn = document.querySelectorAll('.deljob')
-const requestedItem = document.querySelectorAll('span.not')
-const itemComplete = document.querySelectorAll('span.completed')
+const deleteJobBtn = document.querySelectorAll('.deljob')``
 const deleteBuildBtn = document.querySelectorAll('.delBuild')
 
 
@@ -12,13 +10,9 @@ Array.from(deleteBtn).forEach((el)=>{
 Array.from(deleteJobBtn).forEach((el)=>{
     el.addEventListener('click', deleteJob)
 })
-Array.from(requestedItem).forEach((el)=>{
-    el.addEventListener('click', markComplete)
-})
 
-Array.from(itemComplete).forEach((el)=>{
-    el.addEventListener('click', markIncomplete)
-})
+
+
 Array.from(deleteBuildBtn).forEach((el)=>{
     el.addEventListener('click', deleteBuild)
 })
@@ -77,44 +71,7 @@ async function deleteBuild(){
     }
 }
 
-async function markComplete(){
-    const requestId = this.parentNode.dataset.id
-    try{
-        const response = await fetch('request/markComplete', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'requestIdFromJsFile': requestId
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
-    }
-}
-
-async function markIncomplete(){
-    const requestId = this.parentNode.dataset.id
-    try{
-        const response = await fetch('request/markIncomplete', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'requestIdFromJsFile': requestId
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
-    }
-}
-
-
-  //menu button logic, click to open menu
+  //nav menu button logic, click to open menu
   const navMenu = document.querySelector('#menuButton')
   const menuNav = document.querySelector('.menuNav')
   navMenu.addEventListener('click',e=>{
